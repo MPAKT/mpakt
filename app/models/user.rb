fb60e22d80a605d2983f12f7cf0c18a10fc19393
egglestn <segglestn@gmail.com>
2019-08-20 10:30:16 +0100

    Can't make the enum work

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, and :omniauthable
   devise :database_authenticatable, :registerable, :trackable,
          :recoverable, :rememberable, :validatable
-
-  #enum moderator %i[
-  #  none
-  #  posts
-  #  posts_and_users
-  #]
 end
