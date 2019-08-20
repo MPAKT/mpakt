83aff9101338b07f1fb6aa8cc16e60c9322b6eba
egglestn <segglestn@gmail.com>
2019-08-20 09:44:15 +0100

    Update tests to match

--- spec/features/policy_spec.rb
+++ spec/features/policy_spec.rb
@@ -36,8 +36,8 @@ RSpec.feature "Ts and Cs" do
         exclude_keys: [:us_1_html, :cookies_1_html]
       )
 
-      expect(page).to have_content "This policy, together with our Terms and Conditions sets out the basis on which any data we collect from you, or that you provide to us, will be processed by us. Please read the following carefully to understand our views and practices regarding your personal data and how we will treat it. By visiting DIPpy you are accepting and consenting to the practices described in this policy."
-      expect(page).to have_content "Our website uses cookies to distinguish you from other users of our website. This helps us to provide you with a good experience when you browse our website and also allows us to improve our site. For detailed information on the cookies we use and the purposes for which we use them see our Cookie policy"
+      expect(page).to have_content "This policy, together with our Terms and Conditions sets out how we process your data. Please read it carefully. By visiting DIPpy you accept and consent to the practices described in this policy."
+      expect(page).to have_content "Our website uses cookies to distinguish you from other users of our website. This helps us to provide a good experience when you browse our website. For detailed information on the cookies we use see our Cookie policy"
     end
   end
 end
