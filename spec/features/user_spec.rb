5e92c4a9c8d4c0b4e4b8e8730e3d353e69f22be1
egglestn <segglestn@gmail.com>
2019-08-20 16:36:13 +0100

    Feature test for sign out and sign in

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,20 +1,51 @@
 require "rails_helper"
 
 RSpec.feature "User" do
-  context "As an user" do
+  context "As a user" do
     scenario "I can sign up and sign in" do
-      visit "/users/sign_up"
+      visit "/"
+
+      within ".menu" do
+        click_on I18n.t("layouts.header.sign_in")
+      end
+
+      within ".devise-links" do
+        click_on I18n.t("devise.shared.links.sign_up")
+      end
 
       within ".new_user" do
         fill_in :user_email, with: "user@dippy.com"
         fill_in :user_password, with: "123456"
         fill_in :user_password_confirmation, with: "123456"
 
-        click_on "Sign up"
+        click_on I18n.t("layouts.header.sign_up")
       end
 
       within ".flash" do
-        expect(page).to have_content "Welcome! You have signed up successfully"
+        expect(page).to have_content I18n.t("devise.registrations.signed_up")
+      end
+
+      within ".menu" do
+        click_on I18n.t("layouts.header.exit")
+      end
+
+      within ".flash" do
+        expect(page).to have_content I18n.t("devise.sessions.signed_out")
+      end
+
+      within ".menu" do
+        click_on I18n.t("layouts.header.sign_in")
+      end
+
+      within ".new_user" do
+        fill_in :user_email, with: "user@dippy.com"
+        fill_in :user_password, with: "123456"
+
+        click_on I18n.t("devise.sessions.new.sign_in")
+      end
+
+      within ".flash" do
+        expect(page).to have_content I18n.t("devise.sessions.signed_in")
       end
     end
   end
