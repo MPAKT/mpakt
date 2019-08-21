2ffb01a4df86dbb86f521ef21a5400e04fb3f08a
egglestn <segglestn@gmail.com>
2019-08-21 10:00:29 +0100

    Update thredded initialiser

--- db/migrate/20190821085131_update_users.rb
+++ db/migrate/20190821085131_update_users.rb
@@ -1,7 +1,7 @@
 class UpdateUsers < ActiveRecord::Migration[5.2]
   def change
     remove_column :users, :moderator, :integer
-    add_column :users, :moderator, :boolean
-    add_column :users, :admin, :boolean
+    add_column :users, :moderator, :boolean, default: false
+    add_column :users, :admin, :boolean, default: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -63,8 +63,8 @@ ActiveRecord::Schema.define(version: 2019_08_21_085131) do
     t.string "short_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "moderator"
-    t.boolean "admin"
+    t.boolean "moderator", default: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
