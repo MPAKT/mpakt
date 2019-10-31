a970130fdf15f2d4a83a1c340fdce0a2f1114a67
egglestn <segglestn@gmail.com>
2019-10-31 15:19:44 +0000

    Change emojis text to smiley icon

--- app/javascript/emoji.js
+++ app/javascript/emoji.js
@@ -8,20 +8,26 @@ const Emoji = () => {
     }
   });
 
-  $(document).on('click', '.emojis', event => {
+  $(document).on('click', '.emoji-btn', event => {
     event.preventDefault();
   });
 
   const initButton = () => {
-    const $emojiButton = document.createElement("button");
-    $emojiButton.className = "emojis btn"
-    $emojiButton.innerHTML = "Emojis"
+    const $emojiButton = document.createElement("a");
+    $emojiButton.className = "emoji-btn btn"
+    const $emojiHolder = document.createElement("span");
+    $emojiHolder.className = "emoji-icon"
+    $emojiHolder.innerHTML = "<img src='/assets/images/smiley.png'/>"
+    $emojiButton.append($emojiHolder);
+
     $(".blog_summary").append($emojiButton);
     const picker = new EmojiButton();
 
     picker.on('emoji', emoji => {
       console.log("Selected emoji")
       console.log(emoji)
+      //var emojiString = "<span class=\"emoji\">" + emoji + "</span>"
+      //console.log(emojiString)
       $(".blog_summary").find('input')[0].value += emoji;
     });
 
