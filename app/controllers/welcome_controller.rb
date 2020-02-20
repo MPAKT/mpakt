32136e43bbfc784a9bc89f18ec8b98e564c56988
egglestn <segglestn@gmail.com>
2020-02-20 10:28:18 +0000

    Refactor landing page to work for signed in users

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class WelcomeController < ApplicationController
-  def index; end
+  def index
+    @forum_post = Thredded::Post.order(created_at: :desc).first if current_user
+  end
 end
